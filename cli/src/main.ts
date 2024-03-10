62601b36176aef7c62bce2d0424f79426b400ab0
Pierre beucher <pierre@crafteo.io>
2024-03-10 15:39:01 +0100

    feat: stop/start/restart commands

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -42,6 +42,25 @@ program.command('get <name>')
     await getBoxDetails(name)
   });
 
+program.command('stop <name>')
+  .description('Stop an instance')
+  .action(async (name: string) => {
+    await stop(name)
+  });
+
+program.command('start <name>')
+  .description('Start an instance')
+  .action(async (name: string) => {
+    await start(name)
+  });
+
+program.command('restart <name>')
+  .description('Restart an instance')
+  .action(async (name: string) => {
+    await restart(name)
+  });
+
+  
 program.command('list')
   .description('List all instances')
   .action(() => {
@@ -71,17 +90,17 @@ program.exitOverride((e) => {
 async function deploy(boxName: string) {
     console.info(`${emoji.get("rocket")} Deploying box: ${boxName}...`)
     const bm = getBoxManager(boxName)
-    await bm.deploy()
+    const o = await bm.deploy()
     console.info(`${emoji.get("white_check_mark")} Deployed: ${boxName}`)
-    const outputs = await bm.get()
-    console.info(JSON.stringify(outputs))
+    console.info(JSON.stringify(o, undefined, 2))
 }
 
 async function provision(boxName: string) {
   console.info(`${emoji.get("gear")} Provisioning box: ${boxName}...`)
   const bm = getBoxManager(boxName)
-  await bm.provision()
+  const o = await bm.provision()
   console.info(`${emoji.get("white_check_mark")} Provisioned: ${boxName}...`)
+  console.info(JSON.stringify(o, undefined, 2))
 }
 
 async function preview(boxName: string) {
@@ -94,7 +113,28 @@ async function destroy(boxName: string) {
   console.info(`${emoji.get("bomb")} Destroying box: ${boxName}...`)
   const bm = getBoxManager(boxName)
   await bm.destroy()
-  console.info(`${emoji.get("boom")} Destroyed ${boxName}...`)
+  console.info(`${emoji.get("boom")} Destroyed ${boxName}`)
+}
+
+async function restart(boxName: string) {
+  console.info(`${emoji.get("recycle")} Restarting box: ${boxName}...`)
+  const bm = getBoxManager(boxName)
+  await bm.reboot()
+  console.info(`${emoji.get("white_check_mark")} Restarted ${boxName}`)
+}
+
+async function start(boxName: string) {
+  console.info(`${emoji.get("rocket")} Starting box: ${boxName}...`)
+  const bm = getBoxManager(boxName)
+  await bm.start()
+  console.info(`${emoji.get("white_check_mark")} Started ${boxName}...`)
+}
+
+async function stop(boxName: string) {
+  console.info(`${emoji.get("stop_sign")} Stopping box: ${boxName}...`)
+  const bm = getBoxManager(boxName)
+  await bm.stop()
+  console.info(`${emoji.get("white_check_mark")} Stopped ${boxName}`)
 }
 
 async function getWolfPin(boxName: string){
