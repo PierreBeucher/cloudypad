32bbb300cdc81c38cff9a35e3ea599f1130b8a7b
Pierre beucher <pierre@crafteo.io>
2024-04-20 13:49:30 +0200

    chore: add plandex to nix flake (experimental)

--- flake.nix
+++ flake.nix
@@ -8,6 +8,26 @@
     flake-utils.lib.eachDefaultSystem (system:
       let  
         pkgs = nixpkgs.legacyPackages.${system}; 
+        plandex = pkgs.stdenv.mkDerivation {
+          name = "plandex";
+          
+          src = builtins.fetchurl {
+            url = "https://github.com/plandex-ai/plandex/releases/download/cli/v0.8.3/plandex_0.8.3_linux_amd64.tar.gz";
+            sha256 = "sha256:1bgxm3lhvs02jwmw3918j63cd0ycyykmbddsbwvyz4wjbvyx1mlz";
+          };
+
+          unpackPhase = "tar -zxvf $src";
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r * $out/bin
+          '';
+
+          meta = {
+            description = "Plandex CLI";
+            homepage = "https://github.com/plandex-ai/plandex";
+          };
+        };
       in {
         devShells = {
           default = pkgs.mkShell {
@@ -19,6 +39,7 @@
               go-task
               gettext
               typescript
+              plandex
             ];
             
             shellHook = ''
