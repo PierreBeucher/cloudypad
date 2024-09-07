602ec55060286e6a5041f8202650fd233e6d7e8e
Pierre beucher <pierre@crafteo.io>
2024-09-07 14:04:19 +0200

    chore: ensure devices are all setup on boot
    seems that using .run installer instead of PPA package cause some devices to not load on boot. Using this workaround, which may be the final solution or until something better can be implemented.

--- ansible/roles/nvidia-driver/files/nvidia-setup-devices.sh
+++ ansible/roles/nvidia-driver/files/nvidia-setup-devices.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-#
+
+# Some required /dev/nvidia* files may not be created on boot
+# Running a few command in user space cause them to appear
+# This script is configured as a service to be run at boot time to ensure every needed nvidia devices are created properly
+
 # Ensure /dev/nvidia-caps/* devices are present
 # Used by nvidia-setup-caps service to ensure MIG devices are present on boot which is not always the case (eg. AWS G5 instance)
 # See https://docs.nvidia.com/datacenter/tesla/mig-user-guide/index.html#device-nodes-devfs
@@ -10,3 +14,15 @@ if [ $? -ne 0 ]; then
   echo "Error: nvidia-modprobe command failed. Could not ensure /dev/nvidia-caps/* existence" >&2
   exit 1
 fi
+
+echo "Ensuring other NVIDIA devices are loaded"
+
+# /dev/nvidia-modeset
+mknod /dev/nvidia-modeset c 195 254
+
+# other /dev/nvidia* devices
+# /dev/nvidia-uvm
+# /dev/nvidia-uvm-tools
+# /dev/nvidia0
+# /dev/nvidiactl
+nvidia-smi
