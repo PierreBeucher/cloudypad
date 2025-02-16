546940ecbc4137a0919b69e0e7a1d936ae3e637b
Pierre beucher <pierre@crafteo.io>
2025-02-16 20:17:09 +0100

    chore: prepare release 0.17.0 - update version in package files and scripts

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let  
         pkgs = import nixpkgs { system = system; config.allowUnfree = true; };
-        cloudypadVersion = "0.16.0";
+        cloudypadVersion = "0.17.0";
       in {
         packages = rec {
           default = cloudypad;
@@ -18,7 +18,7 @@
 
             src = pkgs.fetchurl {
               url = "https://raw.githubusercontent.com/PierreBeucher/cloudypad/v${cloudypadVersion}/cloudypad.sh";
-              hash = "sha256:1n6pm99jyfp1wzk0rii7h3dp36wdaka0jikw8vbbnw1b056wlh36";
+              hash = "sha256:1gh0yh270a034m5qff9vz984jh7cgq1b2qbcrlxzdpdahk996vhq";
             };
 
             phases = [ "installPhase" ];
