c0f43aaf7fdf71171255fb035e47ff4c25fe972c
Pierre beucher <pierre@crafteo.io>
2025-02-26 15:27:44 +0100

    chore: enable back commented test

--- test/unit/core/initializer.spec.ts
+++ test/unit/core/initializer.spec.ts
@@ -1,10 +1,11 @@
 import * as assert from 'assert';
-import { GcpInstanceInput } from '../../../src/providers/gcp/state';
-import { CLOUDYPAD_PROVIDER_GCP, PUBLIC_IP_TYPE_STATIC } from '../../../src/core/const';
+import { GcpInstanceInput, GcpInstanceStateV1 } from '../../../src/providers/gcp/state';
+import { CLOUDYPAD_CONFIGURATOR_ANSIBLE, CLOUDYPAD_PROVIDER_GCP, PUBLIC_IP_TYPE_STATIC } from '../../../src/core/const';
 import { DEFAULT_COMMON_INPUT } from '../utils';
 import { InteractiveInstanceInitializer } from '../../../src/cli/initializer';
 import { GcpCreateCliArgs, GcpInputPrompter } from '../../../src/providers/gcp/cli';
 import { STREAMING_SERVER_SUNSHINE } from '../../../src/cli/prompter';
+import { StateLoader } from '../../../src/core/state/loader';
 
 describe('Instance initializer', () => {
 
@@ -48,7 +49,9 @@ describe('Instance initializer', () => {
         costAlert: false,
         streamingServer: STREAMING_SERVER_SUNSHINE,
         sunshineUser: TEST_INPUT.configuration.sunshine?.username,
-        sunshinePassword: TEST_INPUT.configuration.sunshine?.passwordBase64,
+        sunshinePassword: TEST_INPUT.configuration.sunshine?.passwordBase64 ? Buffer.from(TEST_INPUT.configuration.sunshine.passwordBase64, 'base64').toString('utf-8') : undefined,
+        sunshineImageRegistry: TEST_INPUT.configuration.sunshine?.imageRegistry,
+        sunshineImageTag: TEST_INPUT.configuration.sunshine?.imageTag,
     }
 
     const TEST_CLI_ARGS_ALREADY_EXISTING: GcpCreateCliArgs = {
@@ -57,38 +60,47 @@ describe('Instance initializer', () => {
         overwriteExisting: false,
     }
 
-    // // Check instanceInitializer creates instance state as expected
-    // // Testing here using GCP state, but Initializer is generic and should work with any statet
-    // it('should initialize instance state with provided arguments', async () => {
+    // Check instanceInitializer creates instance state as expected
+    // Testing here using GCP state, but Initializer is generic and should work with any statet
+    it('should initialize instance state with provided arguments', async () => {
 
-    //     await new InteractiveInstanceInitializer({ 
-    //         provider: CLOUDYPAD_PROVIDER_GCP,
-    //         inputPrompter: new GcpInputPrompter()
-    //     }).initializeInstance(TEST_CLI_ARGS, { skipPostInitInfo: true })
+        await new InteractiveInstanceInitializer({ 
+            provider: CLOUDYPAD_PROVIDER_GCP,
+            inputPrompter: new GcpInputPrompter()
+        }).initializeInstance(TEST_CLI_ARGS, { skipPostInitInfo: true })
 
-    //     // Check state has been written
-    //     const state = await new StateLoader().loadAndMigrateInstanceState(instanceName)
+        // Check state has been written
+        const state = await new StateLoader().loadAndMigrateInstanceState(instanceName)
 
-    //     const expectState: GcpInstanceStateV1 = {
-    //         version: "1",
-    //         name: instanceName,
-    //         provision: {
-    //             provider: CLOUDYPAD_PROVIDER_GCP,
-    //             input: TEST_INPUT.provision,
-    //             output: {
-    //                 host: "127.0.0.1",
-    //                 instanceName: "dummy-gcp"
-    //             }
-    //         },
-    //         configuration: {
-    //             configurator: CLOUDYPAD_CONFIGURATOR_ANSIBLE,
-    //             input: {},
-    //             output: {}
-    //         }
-    //     }
+        const expectState: GcpInstanceStateV1 = {
+            version: "1",
+            name: instanceName,
+            provision: {
+                provider: CLOUDYPAD_PROVIDER_GCP,
+                input: TEST_INPUT.provision,
+                output: {
+                    host: "127.0.0.1",
+                    instanceName: "dummy-gcp"
+                }
+            },
+            configuration: {
+                configurator: CLOUDYPAD_CONFIGURATOR_ANSIBLE,
+                input: {
+                    sunshine: {
+                        enable: DEFAULT_COMMON_INPUT.configuration.sunshine?.enable ?? false,
+                        username: DEFAULT_COMMON_INPUT.configuration.sunshine?.username ?? "",
+                        passwordBase64: DEFAULT_COMMON_INPUT.configuration.sunshine?.passwordBase64 ?? "",
+                        imageTag: DEFAULT_COMMON_INPUT.configuration.sunshine?.imageTag ?? "",
+                        imageRegistry: DEFAULT_COMMON_INPUT.configuration.sunshine?.imageRegistry
+                    },
+                    wolf: null
+                },
+                output: {}
+            }
+        }
         
-    //     assert.deepEqual(state, expectState)
-    // })
+        assert.deepEqual(state, expectState)
+    })
 
     it('should failed to initialize for existing instance with no overwrite', async () => {
 
