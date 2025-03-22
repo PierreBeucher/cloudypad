2aa5c62604151f9dbdbeed26852294e073dce70e
Pierre beucher <pierre@crafteo.io>
2025-03-22 12:50:26 +0100

    fix: prevent pulseaudio from quitting before over components starts

--- containers/sunshine/overlay/cloudy/bin/start-pulse-audio.sh
+++ containers/sunshine/overlay/cloudy/bin/start-pulse-audio.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-pulseaudio --log-target=stderr --log-time
+# pulseaudio will quit when idle but it somehow prevent Sunshine streaming from working
+# ensure it keeps running with exit-idle-time=-1
+pulseaudio --log-target=stderr --log-time --exit-idle-time=-1
