50df2a2e40e2e24ae2310db690c000b8b883aad5
Pierre beucher <pierre@crafteo.io>
2025-04-01 12:04:07 +0200

    feat: exit button shows how to exit rather than killing container
    killing container caused game download to interrupt and Moonlight to show error code, providing bad user experience. Rather, provide Moonlight shortcuts for clean exit with instructions and let instance autostop as failsafe.

---
+++ containers/sunshine/overlay/cloudy/bin/stop-container-exit-prompt.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "REMEMBER TO TURN OFF YOUR INSTANCE AFTER EXIT !"
+echo 
+echo "To exit session press"
+echo 
+echo "  PC keyboard: CTRL+ALT+SHIFT+Q"
+echo "  MacOS keyboard: SHIFT+CONTROL+OPTION+Q"
+echo 
+echo "Notes:"
+echo "- Instance will shutdown automatically after 15 min of inactivity. "
+echo "- If you're downloading a game, instance will remain active until download is complete and then turn-off automatically."
+
+sleep 999
