77d56e3be458f5a6fb08d6a97d5b9f656ae2cc36
Pierre beucher <pierre@crafteo.io>
2025-04-20 11:30:27 +0200

    fix: Sunshine password special characters handling
    some password with special characters were not handled properly,
    such as password starting with '@' or '`' which are YAML reserved scalars.
    Also the "quote" Ansible filter didn't behave as expected as it's reserved for shell
    not YAML...
    
    Now passing it all as base64 encoded string and
    decoding as close as possible without passing
    decoded string around.

--- containers/sunshine/overlay/cloudy/bin/start-sunshine.sh
+++ containers/sunshine/overlay/cloudy/bin/start-sunshine.sh
@@ -9,7 +9,8 @@ mkdir -p $CLOUDYPAD_DATA_DIR/sunshine
 envsubst < $CLOUDYPAD_CONF_DIR/sunshine/sunshine.conf.template > $CLOUDYPAD_CONF_DIR/sunshine/sunshine.conf
 
 # Set credentials if both Sunshine Web UI password and username are present
-if [[ -n "$SUNSHINE_WEB_PASSWORD" && -n "$SUNSHINE_WEB_USERNAME" ]]; then
+if [[ -n "$SUNSHINE_WEB_PASSWORD_BASE64" && -n "$SUNSHINE_WEB_USERNAME" ]]; then
+    SUNSHINE_WEB_PASSWORD=$(echo "$SUNSHINE_WEB_PASSWORD_BASE64" | base64 -d)
     sunshine $CLOUDYPAD_CONF_DIR/sunshine/sunshine.conf --creds $SUNSHINE_WEB_USERNAME $SUNSHINE_WEB_PASSWORD
 fi
 
