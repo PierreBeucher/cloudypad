e9f75e8f20a0626d9be456235c2b9f7d02ce63a7
gabriel engvall <zorro.ee.col@gmail.com>
2025-05-20 16:44:59 +0200

    dummy local test

--- src/providers/dummy/state.ts
+++ src/providers/dummy/state.ts
@@ -20,7 +20,7 @@ const DummyProvisionInputV1Schema = CommonProvisionInputV1Schema.extend({
 })
 
 const DummyConfigurationOutputV1Schema = CommonConfigurationOutputV1Schema.extend({
-    configuredAt: z.number().describe("Timestamp (seconds) the instance was finished configured at"),
+    configuredAt: z.number().describe("Timestamp (seconds) the instance was finished configured at").optional(),
     dataDiskConfigured: z.boolean().describe("Whether the data disk was configured")
 })
 
