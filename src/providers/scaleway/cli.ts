0f5e0747db4c89e5b5c4d3aa6147cc4cffba1661
Pierre beucher <pierre@crafteo.io>
2025-05-29 11:53:34 +0200

    feat(scaleway): allow updating base image ID

--- src/providers/scaleway/cli.ts
+++ src/providers/scaleway/cli.ts
@@ -263,6 +263,7 @@ export class ScalewayCliCommandGenerator extends CliCommandGenerator {
             .addOption(CLI_OPTION_KEYBOARD_OPTIONS)
             .addOption(CLI_OPTION_DELETE_INSTANCE_SERVER_ON_STOP)
             .option('--instance-type <instance-type>', 'Instance type')
+            .option('--image-id <image-id>', 'Existing image ID for instance server. Disk size must be equal or greater than image size.')
             .action(async (cliArgs: ScalewayUpdateCliArgs) => {
                 this.analytics.sendEvent(RUN_COMMAND_UPDATE, { provider: CLOUDYPAD_PROVIDER_SCALEWAY })
 
