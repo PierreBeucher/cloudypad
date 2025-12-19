db854a10c9bea80ce9ac27d61169ae34f6c89f45
George <george@fozzie.dev>
2025-12-19 22:20:26 +0000

    Add MoonDeck support

---
+++ containers/sunshine/overlay/cloudy/bin/start-moondeckbuddy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+wait-x-availability.sh
+
+source export-dbus-address.sh
+
+MOONDECKBUDDY_APPIMAGE_PATH="/opt/MoonDeckBuddy/MoonDeckBuddy.AppImage"
+
+if [ ! -x "$MOONDECKBUDDY_APPIMAGE_PATH" ]; then
+    echo "MoonDeckBuddy AppImage not found at $MOONDECKBUDDY_APPIMAGE_PATH"
+    exit 1
+fi
+
+cd "$CLOUDYPAD_USER_HOME"
+
+exec "$MOONDECKBUDDY_APPIMAGE_PATH"
